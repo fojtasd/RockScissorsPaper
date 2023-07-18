30665ee68b2986b20d2c9f93588caa1a1f628a14
fojtasd <dave.fojtas@gmail.com>
2023-07-18 12:26:07 +0200

    add enter variables for user

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import javax.swing.JFrame;
-import java.awt.*;
-
 
 public class Main {
+    /**
+     * Enter desire number of rock, scissors, paper. Entities are in the same ratio.
+     */
+    public static int numberOfObjects = 50;
+
+    /**
+     * Enter desire size of canvas.
+     */
+    public static int widthOfCanvas = 900;
+    public static int heightOfCanvas = 900;
+
     public static void main(String[] args) throws Exception {
         JFrame frame = new JFrame("RockScissorsPaper");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        MovingObjectsPanel movingObjectsPanel = new MovingObjectsPanel(50, 900, 900);
+        MovingObjectsPanel movingObjectsPanel = new MovingObjectsPanel(numberOfObjects, widthOfCanvas, heightOfCanvas);
         frame.getContentPane().add(movingObjectsPanel);
         frame.pack();
         frame.setVisible(true);
